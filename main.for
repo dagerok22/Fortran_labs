0b72aaa4e3c5b8b255e727ef3880fbb761ebee87
Sergey <dagerok22@gmail.com>
2017-02-18 17:34:13 +0700

    Fixed maximums

--- main.for
+++ main.for
@@ -16,16 +16,34 @@
       OPEN(1, FILE='assets/source.txt')
       WRITE(1,5) ' y\x'
       DO x=minX, maxX, step
+        IF(x.GT.maxX) THEN
+        WRITE(1,3) maxX
+        EXIT
+        END IF
       WRITE(1,3) x
       END DO
       WRITE(1, *) ' '
       DO y=minY, maxY, step
         WRITE(1,6) y
         DO x=minX, maxX, step
-          IF(x.GT.maxX) EXIT
+          IF(x.GT.maxX .OR. y.GT.maxY) THEN
+            IF(y.LT.maxY)THEN
+              WRITE(1,2) 1/TAN(ABS(maxX + y))
+              EXIT
+            ELSE
+              WRITE(1,2) 1/TAN(ABS(x + maxY))
+              EXIT
+            END IF
+            IF(x.GT.maxX .AND. y.GT.maxY)THEN
+              WRITE(1,2) 1/TAN(ABS(maxX + maxY))
+              EXIT
+            END IF
+          END IF
           WRITE(1,2) 1/TAN(ABS(x+y))
         END DO
-        IF(y.GT.maxY) EXIT
+        IF(y.GT.maxY) THEN
+        WRITE(1,3) maxY
+        END IF
         WRITE(1, *) ' '
       END DO
 2     FORMAT(' |', F5.1, $)
